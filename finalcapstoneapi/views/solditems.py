fc91b77f1b4f3d7a21bd4a218c2abfacd600bf4e
mswinkels09 <mswinkels09@gmail.com>
2020-12-14 16:03:48 -0600

    full CRUD on expenses, user must fill in every required key to create or update an expense

--- finalcapstoneapi/views/solditems.py
+++ finalcapstoneapi/views/solditems.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework import status
-from finalcapstoneapi.models import Item, Category, Listing_Type, Weight_Type
+from finalcapstoneapi.models import Item
 from finalcapstoneapi.views.listeditems import CategorySerializer, WeightTypeSerializer, ListingTypeSerializer, UserSerializer
 
 class SoldItemSerializer(serializers.ModelSerializer):
