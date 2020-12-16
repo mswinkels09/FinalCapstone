c5d077a939f804c695e3a8c8077e1ac4b27a125d
mswinkels09 <mswinkels09@gmail.com>
2020-12-16 15:20:46 -0600

    profit can now be grouped by listing types

--- finalcapstoneapi/models/listing_type.py
+++ finalcapstoneapi/models/listing_type.py
@@ -4,3 +4,11 @@ from django.db import models
 class Listing_Type(models.Model):
 
     name = models.CharField(max_length=55)
+
+    @property
+    def profit(self, pk=None):
+        return self.__profit
+
+    @profit.setter
+    def profit(self, value):
+        self.__profit=value
